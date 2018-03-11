b7321f159aada7acd80ae63d70b38736b97c5d99
IndrajeetPatil <inderonline1988@gmail.com>
2018-03-10 21:09:42 -0500

    rewrote the combine_plots functions to now allow all three options of caption, title, and annotation

--- R/combine_plots.R
+++ R/combine_plots.R
@@ -1,28 +1,43 @@
-#' @title Combining multiple plots using `cowplot::plot_grid()` with a title and a caption
+#' @title Combining multiple plots using `cowplot::plot_grid()` with a combination of title, caption, and annotation label
 #' @name combine_plots
 #' @author Indrajeet Patil
-#' @description Wrapper around `cowplot::plot_grid()` that will return a plot grid along with a title and a caption
-#' @return Combined plot with title and caption
+#' @description Wrapper around `cowplot::plot_grid()` that will return a plotgrid along with a combination of
+#' title, caption, and annotation label
+#' @return Combined plot with title and/or caption and/or annotation label
 #'
 #' @param ... Additional arguments used in the function `cowplot::plot_grid()`.
 #' @param title.text String or plotmath expression to be drawn as title for the *combined plot*.
-#' @param caption.text String or plotmath expression to be drawn as the captionfor the *combined plot*.
 #' @param title.colour Text color for title.
+#' @param title.size Point size of title text.
+#' @param title.vjust Vertical justification for title. Default = 0.5 (centered on y).
+#' 0 = baseline at y, 1 = ascender at y.
+#' @param title.hjust Horizontal justification for title. Default = 0.5 (centered on x).
+#' 0 = flush-left at x, 1 = flush-right.
+#' @param title.fontface The font face ("plain", "bold", etc.) for title.
+#' @param caption.text String or plotmath expression to be drawn as the caption for the *combined plot*.
 #' @param caption.colour Text color for caption.
 #' @param caption.size Point size of title text.
-#' @param title.size Point size of caption text.
 #' @param caption.vjust Vertical justification for caption. Default = 0.5 (centered on y).
-#' 0 = baseline at y, 1 = ascender at y.
-#' @param title.vjust Vertical justification for title. Default = 0.5 (centered on y).
 #' 0 = baseline at y, 1 = ascender at y.
 #' @param caption.hjust Horizontal justification for caption. Default = 0.5 (centered on x).
 #' 0 = flush-left at x, 1 = flush-right.
-#' @param title.hjust Horizontal justification for title. Default = 0.5 (centered on x).
-#' 0 = flush-left at x, 1 = flush-right.
-#' @param caption.rel.heights Numerical vector of relative columns heights while combining (title, plot, caption).
+#' @param caption.fontface The font face ("plain", "bold", etc.) for caption.
+#' @param sub.text The label with which the *combined plot* should be annotated. Can be a plotmath expression.
+#' @param sub.colour Text color for annotation label.
+#' @param sub.size Point size of annotation text.
+#' @param sub.x The x position of annotation label.
+#' @param sub.y The y position of annotation label.
+#' @param sub.hjust Horizontal justification for annotation label.
+#' @param sub.vjust Vertical justification for annotation label.
+#' @param sub.vpadding Vertical padding. The total vertical space added to the label, given in grid units.
+#' By default, this is added equally above and below the label. However, by changing the y and vjust parameters, this can be changed.
+#' @param sub.fontface The font face ("plain", "bold", etc.) for the annotation label.
+#' @param title.caption.rel.heights Numerical vector of relative columns heights while combining (title, plot, caption).
 #' @param title.rel.heights Numerical vector of relative columns heights while combining (title, plot).
+#' @param caption.rel.heights Numerical vector of relative columns heights while combining (plot, caption).
 #'
 #' @import cowplot
+#' @import grid
 #'
 #' @examples
 #' # loading the necessary libraries
@@ -31,13 +46,15 @@
 #' # preparing the first plot
 #' p1 <-
 #'   ggplot2::ggplot(data = subset(iris, iris$Species == "setosa"),
-#'                   aes(x = Sepal.Length, y = Sepal.Width)) + geom_point() +
+#'                   aes(x = Sepal.Length, y = Sepal.Width)) +
+#'                   geom_point() +
 #'   labs(title = "setosa")
 #'
 #' # preparing the second plot
 #' p2 <-
 #'   ggplot2::ggplot(data = subset(iris, iris$Species == "versicolor"),
-#'                   aes(x = Sepal.Length, y = Sepal.Width)) + geom_point() +
+#'                   aes(x = Sepal.Length, y = Sepal.Width)) +
+#'                   geom_point() +
 #'   labs(title = "versicolor")
 #'
 #' # combining the plot with a title and a caption
@@ -53,21 +70,32 @@
 #'
 #' @export
 
-
 combine_plots <-
   function(...,
-             title.text = NULL,
-             caption.text = NULL,
-             title.colour = "black",
-             caption.colour = "black",
-             caption.size = 10,
-             title.size = 16,
-             caption.vjust = 0.5,
-             caption.hjust = 0.5,
-             title.vjust = 0.5,
-             title.hjust = 0.5,
-             title.rel.heights = c(0.1, 1.2),
-             caption.rel.heights  = c(0.1, 1.2, 0.1)) {
+           title.text = NULL,
+           title.colour = "black",
+           title.size = 16,
+           title.vjust = 0.5,
+           title.hjust = 0.5,
+           title.fontface = "bold",
+           caption.text = NULL,
+           caption.colour = "black",
+           caption.size = 10,
+           caption.vjust = 0.5,
+           caption.hjust = 0.5,
+           caption.fontface = "plain",
+           sub.text = NULL,
+           sub.colour = "black",
+           sub.size = 14,
+           sub.vjust = 0.5,
+           sub.hjust = 0.5,
+           sub.fontface = "plain",
+           sub.x = 0.5,
+           sub.y = 0.5,
+           sub.vpadding = grid::unit(1, "lines"),
+           title.rel.heights = c(0.1, 1.2),
+           caption.rel.heights = c(1.2, 0.1),
+           title.caption.rel.heights  = c(0.1, 1.2, 0.1)) {
     # preparing the basic plot
     plot <- cowplot::plot_grid(...)
 
@@ -77,44 +105,73 @@ combine_plots <-
         cowplot::ggdraw() +
         cowplot::draw_label(
           label = title.text,
-          fontface = "bold",
           colour = title.colour,
           size = title.size,
           vjust = title.vjust,
-          hjust = title.hjust
+          hjust = title.hjust,
+          fontface = title.fontface
         )
-
-      # preparing the caption
-      if (!is.null(caption.text)) {
-        caption <-
-          cowplot::ggdraw() +
-          cowplot::draw_label(
-            label = caption.text,
-            colour = caption.colour,
-            size = caption.size,
-            vjust = caption.vjust,
-            hjust = caption.hjust
-          )
-
-        # combining the basic plot with the title and the caption
-        plot <-
-          cowplot::plot_grid(title,
-            plot,
-            caption,
-            ncol = 1,
-            rel_heights = caption.rel.heights
-          )
-      } else {
-        # combining the basic plot with the title
-        plot <-
-          cowplot::plot_grid(title,
-            plot,
-            ncol = 1,
-            rel_heights = title.rel.heights
-          )
-      }
+    }
+    # preparing the caption
+    if (!is.null(caption.text)) {
+      caption <-
+        cowplot::ggdraw() +
+        cowplot::draw_label(
+          label = caption.text,
+          colour = caption.colour,
+          size = caption.size,
+          vjust = caption.vjust,
+          hjust = caption.hjust,
+          fontface = caption.fontface
+        )
     }
 
-    # return the comibined plot with a title and caption
+    # combining the basic plot with the either title or caption or title and caption
+    if (!is.null(title.text)) {
+      if (!is.null(caption.text)) {
+        # if both title and caption are needed
+        plot <-
+          cowplot::plot_grid(title,
+                             plot,
+                             caption,
+                             ncol = 1,
+                             rel_heights = title.caption.rel.heights)
+      } else {
+        # if only title is needed
+        plot <-
+          cowplot::plot_grid(title,
+                             plot,
+                             ncol = 1,
+                             rel_heights = title.rel.heights)
+      }
+    } else if (!is.null(caption.text)) {
+      # if only caption is needed
+      plot <-
+        cowplot::plot_grid(plot,
+                           caption,
+                           ncol = 1,
+                           rel_heights = caption.rel.heights)
+    }
+
+    # finally adding sub if it's needed
+    if (!is.null(sub.text)) {
+      plot <-
+        cowplot::ggdraw(
+          cowplot::add_sub(
+            plot = plot,
+            label = sub.text,
+            x = sub.x,
+            y = sub.y,
+            vpadding = sub.vpadding,
+            colour = sub.colour,
+            size = sub.size,
+            vjust = sub.vjust,
+            hjust = sub.hjust,
+            fontface = sub.fontface
+          )
+        )
+    }
+
+    # return the final, combined plot
     return(plot)
   }
