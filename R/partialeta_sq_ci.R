d2fa9150f568199d4b84893a283fb6c693e1b527
IndrajeetPatil <inderonline1988@gmail.com>
2018-03-21 12:42:41 -0400

    updated function partial_eta_sq to account for more complex designs

--- R/partialeta_sq_ci.R
+++ R/partialeta_sq_ci.R
@@ -12,9 +12,6 @@
 #'
 #' @import dplyr
 #'
-#' @importFrom purrr map
-#' @importFrom tidyr nest
-#' @importFrom tidyr unnest
 #' @importFrom sjstats eta_sq
 #' @importFrom stats anova
 #' @importFrom stats na.omit
@@ -25,7 +22,7 @@
 #' @examples
 #' library(datasets)
 #' library(stats)
-#' x <- stats::lm(data = iris, formula = Sepal.Length ~ Species)
+#' x <- stats::lm(formula = wt ~ mpg*cyl, data = mtcars)
 #' ipmisc::partialeta_sq_ci(lm_object = x, conf.level = 0.95)
 #'
 #' @export
@@ -55,74 +52,76 @@ utils::globalVariables(
     "p75",
     "sd",
     "significance",
-    "type"
+    "type",
+    "conf.high",
+    "conf.low",
+    "partial.etasq",
+    "term"
   )
 )
 
-partialeta_sq_ci <- function(lm_object, conf.level = 0.95, nboot = 1000) {
-  # get the linear model object and turn it into a matrix and turn row names into a variable called "effect"
-  # compute partial eta-squared for each effect
-  # add additional columns containing data and formula that was used to create these effects
-  # details from the anova results
-  aov_df <-
-    as.data.frame(as.matrix(stats::anova(object = lm_object))) %>%
-    tibble::rownames_to_column(df = ., var = "effect")
-  # create a new column for residual degrees of freedom
-  aov_df$df2 <- aov_df$Df[aov_df$effect == "Residuals"]
+# defining the function body
+partialeta_sq_ci <-
+  function(lm_object,
+           conf.level = 0.95,
+           nboot = 1000) {
+    # get the linear model object and turn it into a matrix and turn row names into a variable called "effect"
+    # compute partial eta-squared for each effect
+    # add additional columns containing data and formula that was used to create these effects
+    # details from the anova results
+    aov_df <-
+      as.data.frame(as.matrix(stats::anova(object = lm_object))) %>%
+      tibble::rownames_to_column(df = ., var = "effect")
+    # create a new column for residual degrees of freedom
+    aov_df$df2 <- aov_df$Df[aov_df$effect == "Residuals"]
 
-  aov_df <- aov_df %>%
-    dplyr::select(
-      .data = .,
-      -c(base::grep(pattern = "Sq", x = names(.)))
+    aov_df <- aov_df %>%
+      dplyr::select(.data = .,
+                    -c(base::grep(pattern = "Sq", x = names(.)))) %>%
+      dplyr::rename(.data = .,
+                    df1 = Df) %>% # rename to something more meaningful and tidy
+      stats::na.omit(.) # remove NAs, which would remove the row containing Residuals (redundant at this point)
+
+    # other supplementary information about the results (data and formula used, etc.) can be composed from
+    # sjstats::eta_sq function that now provides 95% CI as well
+
+    # creating dataframe of effect size and its CI with sjstats
+    etasq_df <- sjstats::eta_sq(
+      model = stats::anova(object = lm_object),
+      partial = TRUE,
+      ci.lvl = conf.level,
+      n = nboot
     ) %>%
-    dplyr::rename(
-      .data = .,
-      df1 = Df
-    ) %>% # rename to something more meaningful and tidy
-    stats::na.omit(.) # remove NAs, which would remove the row containing Residuals (redundant at this point)
+      dplyr::rename(
+        .data = .,
+        effect = term,
+        effsize = partial.etasq,
+        LL = conf.low,
+        UL = conf.high
+      )
 
-  # other supplementary information about the results (data and formula used, etc.) can be composed from
-  # sjstats::eta_sq function that now provides 95% CI as well
+    # combining the dataframes (erge the two preceding pieces of information by the common element of Effect
+    combined_df <- dplyr::left_join(x = aov_df,
+                                    y = etasq_df,
+                                    by = "effect") %>% # adding information about dataframe and formula call
+      dplyr::mutate(
+        .data = .,
+        data = as.character(lm_object$call[3]),
+        formula = as.character(lm_object$call[2])
+      ) %>% # reordering columns
+      dplyr::select(.data = .,
+                    data,
+                    formula,
+                    effect,
+                    `F value`,
+                    df1,
+                    df2,
+                    `Pr(>F)`,
+                    effsize,
+                    LL,
+                    UL) %>%
+      tibble::as_data_frame(x = .)
 
-  # creating dataframe of effect size and its CI with sjstats
-  etasq_df <- sjstats::eta_sq(
-    model = stats::anova(object = lm_object),
-    partial = TRUE,
-    ci.lvl = conf.level,
-    n = nboot
-  )
-
-  # extracting statistically important information
-  supp_df <- cbind.data.frame(
-    "effect" = etasq_df$term[[1]],
-    "effsize" = etasq_df$partial.etasq[[1]],
-    "LL" = etasq_df$partial.etasq[[1]],
-    "UL" = etasq_df$partial.etasq[[1]],
-    "data" = as.character(lm_object$call[3]),
-    "formula" = as.character(lm_object$call[2])
-  )
-
-  # combining the dataframes (erge the two preceding pieces of information by the common element of Effect
-  combined_df <-
-    dplyr::left_join(
-      x = aov_df,
-      y = supp_df,
-      by = "effect"
-    ) %>%
-    dplyr::select(
-      .data = .,
-      data,
-      formula,
-      effect,
-      `F value`,
-      df1,
-      df2,
-      `Pr(>F)`,
-      effsize,
-      LL,
-      UL
-    )
-
-  # returning the final dataframe
-  return(combined_df)
-}
+    # returning the final dataframe
+    return(combined_df)
+  }
--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Status](https://ci.appveyor.com/api/projects/status/github/IndrajeetPatil/ipmisc
 [![Coverage
 Status](https://img.shields.io/codecov/c/github/IndrajeetPatil/ipmisc/master.svg)](https://codecov.io/github/IndrajeetPatil/ipmisc?branch=master)
 [![Licence](https://img.shields.io/badge/licence-GPL--3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.en.html)
-[![Last-changedate](https://img.shields.io/badge/last%20change-2018--03--10-yellowgreen.svg)](/commits/master)
+[![Last-changedate](https://img.shields.io/badge/last%20change-2018--03--21-yellowgreen.svg)](/commits/master)
 [![lifecycle](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
 [![minimal R
 version](https://img.shields.io/badge/R%3E%3D-3.2.0-6666ff.svg)](https://cran.r-project.org/)
@@ -35,9 +35,15 @@ devtools::install_github("IndrajeetPatil/ipmisc")
 For documentation, please check out the vignettes folder of the package.
 
 Note that the current package build is faling because the function
-`ipmisc::summary_skim` depends on the development version of `skimr`,
-which can be downloaded here:
+`ipmisc::summary_skim` and `ipmisc::partialeta_sq_ci`depend on the
+development version of `skimr` and `sjstats`, which can be downloaded
+here:
 
 ``` r
+# library(devtools)
+# skimr
 devtools::install_github("ropenscilabs/skimr")
+
+# sjstats
+devtools::install_github("strengejacke/sjstats")
 ```
