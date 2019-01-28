10270597edb23984e5713948a0d88fdefbf08c27
Indrajeet Patil <inderonline1988@gmail.com>
2019-01-28 10:17:40 -0500

    removing set_cwd function

--- R/transpose_df.R
+++ R/transpose_df.R
@@ -1,9 +1,10 @@
-#'
 #' @title Transposing a dataframe while preserving row and column names
 #' @name transpose_df
 #' @author Indrajeet Patil
-#' @description Although functions like `data.table::transpose()` or `base::t()` can transpose dataframes well, they
-#' don't preserve row and column names. The current function transposes dataframes while preserving row and column names.
+#' @description Although functions like `data.table::transpose()` or `base::t()`
+#'   can transpose dataframes well, they don't preserve row and column names.
+#'   The current function transposes dataframes while preserving row and column
+#'   names.
 #' @return A transposed dataframes with row and column names.
 #'
 #' @param df Dataframe to tranpose.
@@ -20,26 +21,31 @@
 #' transpose_df(df = data)
 #' @export
 
-transpose_df <-
-  function(df,
-             var = "rowname",
-             ignore.empty = FALSE) {
-    # transpose the dataframe
-    t_df <-
-      data.table::transpose(
-        l = df,
-        fill = NA,
-        # used to fill shorter list elements so as to return each element of
-        # the transposed result of equal lengths
-        ignore.empty = ignore.empty
-      )
-    # assign the row and column names of the original dataframe to the new dataframe
-    colnames(t_df) <- rownames(df)
-    rownames(t_df) <- colnames(df)
-    # convert the row names into a new column and convert it to a tibble dataframe
-    t_df <- t_df %>%
-      tibble::rownames_to_column(df = ., var = var) %>%
-      tibble::as_data_frame(x = .)
-    # return the fina, transposed dataframe
-    return(t_df)
-  }
+# function body
+transpose_df <- function(df,
+                         var = "rowname",
+                         ignore.empty = FALSE) {
+  # transpose the dataframe
+  t_df <-
+    data.table::transpose(
+      l = df,
+      fill = NA,
+      # used to fill shorter list elements so as to return each element of
+      # the transposed result of equal lengths
+      ignore.empty = ignore.empty
+    )
+
+  # assign the row and column names of the original dataframe to the new
+  # dataframe
+  colnames(t_df) <- rownames(df)
+  rownames(t_df) <- colnames(df)
+
+  # convert the row names into a new column and convert it to a tibble
+  # dataframe
+  t_df <- t_df %>%
+    tibble::rownames_to_column(., var = var) %>%
+    tibble::as_data_frame(x = .)
+
+  # return the fina, transposed dataframe
+  return(t_df)
+}
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 # ipmisc
 
-[![packageversion](https://img.shields.io/badge/Package%20version-0.1.1-orange.svg?style=flat-square)](commits/master)
+[![packageversion](https://img.shields.io/badge/Package%20version-0.0.1-orange.svg?style=flat-square)](commits/master)
 [![Travis Build
 Status](https://travis-ci.org/IndrajeetPatil/ipmisc.svg?branch=master)](https://travis-ci.org/IndrajeetPatil/ipmisc)
 [![AppVeyor Build
@@ -11,15 +11,15 @@ Status](https://ci.appveyor.com/api/projects/status/github/IndrajeetPatil/ipmisc
 [![Coverage
 Status](https://img.shields.io/codecov/c/github/IndrajeetPatil/ipmisc/master.svg)](https://codecov.io/github/IndrajeetPatil/ipmisc?branch=master)
 [![Licence](https://img.shields.io/badge/licence-GPL--3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.en.html)
-[![Last-changedate](https://img.shields.io/badge/last%20change-2018--03--21-yellowgreen.svg)](/commits/master)
+[![Last-changedate](https://img.shields.io/badge/last%20change-2019--01--28-yellowgreen.svg)](/commits/master)
 [![lifecycle](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
 [![minimal R
-version](https://img.shields.io/badge/R%3E%3D-3.2.0-6666ff.svg)](https://cran.r-project.org/)
+version](https://img.shields.io/badge/R%3E%3D-3.5.0-6666ff.svg)](https://cran.r-project.org/)
 
 ## Overview
 
-The goal of ipmisc is to gather miscellaneous helper functions that have
-been helpful to me in data cleaning and analysis.
+The goal of `ipmisc` is to gather miscellaneous helper functions that
+have been helpful to me in data cleaning and analysis.
 
 ## Installation
 
@@ -28,22 +28,4 @@ You can install `ipmisc` from GitHub with:
 ``` r
 # install.packages("devtools")
 devtools::install_github("IndrajeetPatil/ipmisc")
-```
-
-## Documentation
-
-For documentation, please check out the vignettes folder of the package.
-
-Note that the current package build is faling because the function
-`ipmisc::summary_skim` and `ipmisc::partialeta_sq_ci`depend on the
-development version of `skimr` and `sjstats`, which can be downloaded
-here:
-
-``` r
-# library(devtools)
-# skimr
-devtools::install_github("ropenscilabs/skimr")
-
-# sjstats
-devtools::install_github("strengejacke/sjstats")
 ```
