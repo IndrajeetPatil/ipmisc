cff5ab9251d777f5e3d0c2cd3fb3fde7bcf27770
Indrajeet Patil <inderonline1988@gmail.com>
2021-02-22 16:44:33 +0100

    another operator from zeallot

--- R/long_to_wide_converter.R
+++ R/long_to_wide_converter.R
@@ -8,8 +8,9 @@
 #' This conversion is helpful mostly for repeated measures design, where
 #' removing `NA`s by participant can be a bit tedious.
 #'
-#' It does not make sense to spread the dataframe to wide format when the measure
-#' is not repeated, so if `paired = TRUE`, `spread` argument will be ignored.
+#' It does not make sense to spread the dataframe to wide format when the
+#' measure is not repeated, so if `paired = TRUE`, `spread` argument will be
+#' ignored.
 #'
 #' @param data A dataframe (or a tibble) from which variables specified are to
 #'   be taken. Other data types (e.g., matrix,table, array, etc.) will **not**
@@ -17,13 +18,13 @@
 #' @param x The grouping (or independent) variable from the dataframe `data`.
 #' @param y The response (or outcome or dependent) variable from the
 #'   dataframe `data`.
-#' @param subject.id Relevant in case of repeated measures design (`paired =
-#'   TRUE`, i.e.), it specifies the subject or repeated measures identifier.
-#'   **Important**: Note that if this argument is `NULL` (which is the default),
-#'   the function assumes that the data has already been sorted by such an id by
-#'   the user and creates an internal identifier. So if your data is **not**
-#'   sorted and you leave this argument unspecified, the results can be
-#'   inaccurate.
+#' @param subject.id Relevant in case of repeated measures or within-subjects
+#'   design (`paired = TRUE`, i.e.), it specifies the subject or repeated
+#'   measures identifier. **Important**: Note that if this argument is `NULL`
+#'   (which is the default), the function assumes that the data has already been
+#'   sorted by such an id by the user and creates an internal identifier. So if
+#'   your data is **not** sorted and you leave this argument unspecified, the
+#'   results can be inaccurate.
 #' @param paired Logical that decides whether the experimental design is
 #'   repeated measures/within-subjects or between-subjects. The default is
 #'   `FALSE`.
